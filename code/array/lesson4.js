8e7549edbb5d870d9b2fd1362f80ff85b072d953
yangzhuo <1585171898@qq.com>
2019-09-01 21:57:57 +0800

    update array lesson4

--- code/array/lesson4.js
+++ code/array/lesson4.js
@@ -1,18 +1,21 @@
 // leetcode 89 格雷编码
 
-export default(n) => {
+export default (n) => {
   // 递归函数，用来算输入为n的格雷编码序列
   let make = (n) => {
     if (n === 1) {
-      return ['0','1']
+      return [0, 1]
     } else {
       let prev = make(n - 1)
       let result = []
       let max = Math.pow(2, n) - 1
-      for (let i = 0,len = prev.length; i < len; i++) {
+      for (let i = 0, len = prev.length; i < len; i++) {
         result[i] = `0${prev[i]}`
         result[max - i] = `1${prev[i]}`
       }
+      result.forEach((item, index) => {
+        result[index] = parseInt(item, 2)
+      })
       return result
     }
   }
